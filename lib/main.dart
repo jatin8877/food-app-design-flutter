2ed5ed7867cb90d9d42655dd32a3d140f9505d03
jatin <zaatin8877@gmail.com>
2022-06-05 13:05:07 +0530

    food-app-design

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/bakery_app.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BakeryApp(),
+    ),
+  );
+}
